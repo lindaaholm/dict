c0f8e3e56f020704445a9207a7fb21cc065eaf49
lindaaholm <lindaa.holm@gmail.com>
2022-06-09 11:47:48 +0200

    2.8.4

--- dict.py
+++ dict.py
@@ -5,27 +5,30 @@ conn = psycopg2.connect(
    user="user",
    password="abc123"
 )
-
+#Lists all words
 def read_dict(C):
     cur = C.cursor()
     cur.execute("SELECT id, word, translation FROM dictionary;")
     rows = cur.fetchall()
     cur.close()
     return rows
+#Give the user opportunity to add words to the dictionary
 def add_word(C, word, translation):
     cur = C.cursor()
     cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
     cur.close()
+#Delete words from the dictionary
 def delete_word(C, ID):
     cur = C.cursor()
     cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
     cur.close()
+#Saves the dictionary
 def save_dict(C):
     cur = C.cursor()
     cur.execute("COMMIT;")
     cur.close()
 
-
+#Welcome message with instructions 
 print('''Hello and welcome to the dictionary, available commands:
   add    - add a word
   delete - delete a word
